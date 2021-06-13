92bf464eda6795b03bc95de79f461575f410bae1
Usuario <Cristian>
2021-06-13 15:11:18 -0300

    Task 01 Responsable: Cristian Torrejon Descripcion: -Se crearon las clases Customer, Employee, Office y Payment de la capa del modelo.

---
+++ TRABAJO_FINAL_GRUPO_STROKES/src/main/java/ar/edu/unju/fi/tpfinal/model/Customer.java
@@ -0,0 +1,245 @@
+/**
+ * 
+ */
+package ar.edu.unju.fi.tpfinal.model;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Torrejon Cristian
+ *
+ */
+
+/**
+ * Clase que almacena los datos del cliente
+ */
+
+@Component
+public class Customer {
+
+	//Atributos
+	
+	private int customerNumber;
+	private String customerName;
+	private String contactLastName;
+	private String contactFirstName;
+	private String phone;
+	private String addressLine1;
+	private String addressLine2;
+	private String city;
+	private String state;
+	private String postalCode;
+	private String country;
+	private int salesRepEmployeeNumber;
+	private Double creditLimit;
+	
+	/**
+	 *Contructores 
+	 */
+	public Customer() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	/**
+	 * @param customerNumber
+	 * @param customerName
+	 * @param contactLastName
+	 * @param contactFirstName
+	 * @param phone
+	 * @param addressLine1
+	 * @param addressLine2
+	 * @param city
+	 * @param state
+	 * @param postalCode
+	 * @param country
+	 * @param salesRepEmployeeNumber
+	 * @param creditLimit
+	 */
+	public Customer(int customerNumber, String customerName, String contactLastName, String contactFirstName,
+			String phone, String addressLine1, String addressLine2, String city, String state, String postalCode,
+			String country, int salesRepEmployeeNumber, Double creditLimit) {
+		super();
+		this.customerNumber = customerNumber;
+		this.customerName = customerName;
+		this.contactLastName = contactLastName;
+		this.contactFirstName = contactFirstName;
+		this.phone = phone;
+		this.addressLine1 = addressLine1;
+		this.addressLine2 = addressLine2;
+		this.city = city;
+		this.state = state;
+		this.postalCode = postalCode;
+		this.country = country;
+		this.salesRepEmployeeNumber = salesRepEmployeeNumber;
+		this.creditLimit = creditLimit;
+		
+	//Getters y setters
+	}
+	/**
+	 * @return the customerNumber
+	 */
+	public int getCustomerNumber() {
+		return customerNumber;
+	}
+	/**
+	 * @param customerNumber the customerNumber to set
+	 */
+	public void setCustomerNumber(int customerNumber) {
+		this.customerNumber = customerNumber;
+	}
+	/**
+	 * @return the customerName
+	 */
+	public String getCustomerName() {
+		return customerName;
+	}
+	/**
+	 * @param customerName the customerName to set
+	 */
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+	/**
+	 * @return the contactLastName
+	 */
+	public String getContactLastName() {
+		return contactLastName;
+	}
+	/**
+	 * @param contactLastName the contactLastName to set
+	 */
+	public void setContactLastName(String contactLastName) {
+		this.contactLastName = contactLastName;
+	}
+	/**
+	 * @return the contactFirstName
+	 */
+	public String getContactFirstName() {
+		return contactFirstName;
+	}
+	/**
+	 * @param contactFirstName the contactFirstName to set
+	 */
+	public void setContactFirstName(String contactFirstName) {
+		this.contactFirstName = contactFirstName;
+	}
+	/**
+	 * @return the phone
+	 */
+	public String getPhone() {
+		return phone;
+	}
+	/**
+	 * @param phone the phone to set
+	 */
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	/**
+	 * @return the addressLine1
+	 */
+	public String getAddressLine1() {
+		return addressLine1;
+	}
+	/**
+	 * @param addressLine1 the addressLine1 to set
+	 */
+	public void setAddressLine1(String addressLine1) {
+		this.addressLine1 = addressLine1;
+	}
+	/**
+	 * @return the addressLine2
+	 */
+	public String getAddressLine2() {
+		return addressLine2;
+	}
+	/**
+	 * @param addressLine2 the addressLine2 to set
+	 */
+	public void setAddressLine2(String addressLine2) {
+		this.addressLine2 = addressLine2;
+	}
+	/**
+	 * @return the city
+	 */
+	public String getCity() {
+		return city;
+	}
+	/**
+	 * @param city the city to set
+	 */
+	public void setCity(String city) {
+		this.city = city;
+	}
+	/**
+	 * @return the state
+	 */
+	public String getState() {
+		return state;
+	}
+	/**
+	 * @param state the state to set
+	 */
+	public void setState(String state) {
+		this.state = state;
+	}
+	/**
+	 * @return the postalCode
+	 */
+	public String getPostalCode() {
+		return postalCode;
+	}
+	/**
+	 * @param postalCode the postalCode to set
+	 */
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}
+	/**
+	 * @return the country
+	 */
+	public String getCountry() {
+		return country;
+	}
+	/**
+	 * @param country the country to set
+	 */
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	/**
+	 * @return the salesRepEmployeeNumber
+	 */
+	public int getSalesRepEmployeeNumber() {
+		return salesRepEmployeeNumber;
+	}
+	/**
+	 * @param salesRepEmployeeNumber the salesRepEmployeeNumber to set
+	 */
+	public void setSalesRepEmployeeNumber(int salesRepEmployeeNumber) {
+		this.salesRepEmployeeNumber = salesRepEmployeeNumber;
+	}
+	/**
+	 * @return the creditLimit
+	 */
+	public double getCreditLimit() {
+		return creditLimit;
+	}
+	/**
+	 * @param creditLimit the creditLimit to set
+	 */
+	public void setCreditLimit(Double creditLimit) {
+		this.creditLimit = creditLimit;
+	}
+	
+	//Metodo toString
+	@Override
+	public String toString() {
+		return "Customer [customerNumber=" + customerNumber + ", customerName=" + customerName + ", contactLastName="
+				+ contactLastName + ", contactFirstName=" + contactFirstName + ", phone=" + phone + ", addressLine1="
+				+ addressLine1 + ", addressLine2=" + addressLine2 + ", city=" + city + ", state=" + state
+				+ ", postalCode=" + postalCode + ", country=" + country + ", salesRepEmployeeNumber="
+				+ salesRepEmployeeNumber + ", creditLimit=" + creditLimit + "]";
+	}
+	
+}
