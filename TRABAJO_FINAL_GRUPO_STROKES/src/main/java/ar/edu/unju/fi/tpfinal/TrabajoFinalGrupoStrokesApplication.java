e77c760f5601df750dd9d401e56a8e4c8d9a8210
Diego <Diego@DESKTOP-3D2KRSE>
2021-06-23 19:38:44 -0300

    task: Se creo las clases de la capa de Seguridad usando spring security

--- TRABAJO_FINAL_GRUPO_STROKES/src/main/java/ar/edu/unju/fi/tpfinal/TrabajoFinalGrupoStrokesApplication.java
+++ TRABAJO_FINAL_GRUPO_STROKES/src/main/java/ar/edu/unju/fi/tpfinal/TrabajoFinalGrupoStrokesApplication.java
@@ -5,26 +5,27 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import ar.edu.unju.fi.tpfinal.model.Employee;
+//import ar.edu.unju.fi.tpfinal.model.Employee;
 import ar.edu.unju.fi.tpfinal.service.IEmployeeService;
 
 @SpringBootApplication
-public class TrabajoFinalGrupoStrokesApplication implements CommandLineRunner{
+public class TrabajoFinalGrupoStrokesApplication implements CommandLineRunner {
 	@Autowired
 	IEmployeeService employeeService;
+
 	public static void main(String[] args) {
 		SpringApplication.run(TrabajoFinalGrupoStrokesApplication.class, args);
-		
+
 	}
-	
+
 	@Override
-	public void run(String... args) throws Exception{
-		Employee e1 = new Employee(1000, "Pérez", "Pablo", "x5432", "pablito777@gmail.com", "Precidente");
-		Employee e2 = new Employee(1010, "Méndez", "Roberta", "x2360", "rober1023@gmail.com", "Gerente");
-		Employee e3 = new Employee(1020, "Rossi", "Francisco", "x3239", "fran090@gmail.com", "Vendedor");
-		employeeService.addEmployee(e1);
-		employeeService.addEmployee(e2);
-		employeeService.addEmployee(e3);
+	public void run(String... args) throws Exception {
+//		Employee e1 = new Employee(1000, "Pérez", "Pablo", "x5432", "pablito777@gmail.com", "Precidente");
+//		Employee e2 = new Employee(1010, "Méndez", "Roberta", "x2360", "rober1023@gmail.com", "Gerente");
+//		Employee e3 = new Employee(1020, "Rossi", "Francisco", "x3239", "fran090@gmail.com", "Vendedor");
+//		employeeService.addEmployee(e1);
+//		employeeService.addEmployee(e2);
+//		employeeService.addEmployee(e3);
 	}
-	
+
 }
--- TRABAJO_FINAL_GRUPO_STROKES/src/main/java/ar/edu/unju/fi/tpfinal/controller/PublicController.java
+++ TRABAJO_FINAL_GRUPO_STROKES/src/main/java/ar/edu/unju/fi/tpfinal/controller/PublicController.java
@@ -3,9 +3,13 @@
  */
 package ar.edu.unju.fi.tpfinal.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import ar.edu.unju.fi.tpfinal.service.IProductLineService;
 
 /**
  * @author Diego
@@ -14,9 +18,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/public")
 public class PublicController {
+	@Autowired
+	IProductLineService productLineService;
 	
 	@GetMapping("/index")
-	public String index() {
+	public String index(Model model) {
+		model.addAttribute("productlines", productLineService.getProductLines());
 		return "logout";
 	}
 }
